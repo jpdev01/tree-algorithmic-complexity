d5cf60c1a187a70cc97e5627a1d62ddbb41238df
Joao Pedro Truchinski Borba <joao.borba@asaas.com.br>
2023-06-24 21:35:09 -0300

    upd: adicionando title nos gráficos

--- graph.py
+++ graph.py
@@ -6,17 +6,18 @@ plt.rcParams["figure.autolayout"] = True
 
 columns = ['RN', 'AVL', 'B-1', 'B-5', 'B-10']
 
-df = pd.read_csv("D:\Programas\Git\\arvores-avl-rn-b\casomedio.csv", usecols=columns, sep=";")
+df = pd.read_csv("casomedio.csv", usecols=columns, sep=";")
 
 df.plot()
-
-
 plt.xscale("linear")
 plt.yscale("log")
+plt.title("Gráfico do Caso Médio")
 #plt.show()
 
-dc = pd.read_csv("D:\Programas\Git\\arvores-avl-rn-b\piorcaso.csv", usecols=columns, sep=";")
+dc = pd.read_csv("piorcaso.csv", usecols=columns, sep=";")
+
 dc.plot()
 plt.xscale("linear")
 plt.yscale("log")
+plt.title("Gráfico do Pior Caso")
 plt.show()
